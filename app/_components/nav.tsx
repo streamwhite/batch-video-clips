decfdf772c928fc6c4beef688521a52caf96bf75
streamwhite <streamwhite@hotmail.com>
2024-08-06 18:08:46 +0800

    add nav

---
+++ app/_components/nav.tsx
@@ -0,0 +1,24 @@
+// nav components
+export default function Nav() {
+  return (
+    <nav className='nav'>
+      <ul>
+        <li>
+          <a href='/'>Home</a>
+        </li>
+        <li>
+          <a href='/convert'>Convert</a>
+        </li>
+        <li>
+          <a href='/compress'>Compress</a>
+        </li>
+        <li>
+          <a href='/screenshots'>Screenshots</a>
+        </li>
+        <li>
+          <a href='/merge-cta'>Merge CTA</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
